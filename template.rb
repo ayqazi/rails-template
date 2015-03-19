c62e8cab8aa05c7446f8d84bb734c24c1353171b
Asfand Yar Qazi <ayqazi@gmail.com>
2015-03-19 23:28:26 +0000

    Add bootstrap

--- template.rb
+++ template.rb
@@ -5,15 +5,35 @@ end
 run "echo \"source 'https://rubygems.org'\" > Gemfile"
 gem "rails", "~> 4.2.1"
 gem "pg"
-gem "haml-rails"
-gem "sass-rails", "~> 5.0"
-gem "uglifier", ">= 1.3.0"
 
+gem "haml-rails"
+
+gem "uglifier", ">= 1.3.0"
 gem "jquery-rails"
+gem "sass-rails", "~> 5.0"
+gem "bootstrap-sass", "~> 3.3.4"
+
 
 gem_group :development, :test do
   gem "byebug"
   gem "rspec-rails"
+end
+
+FileUtils.rm "app/assets/stylesheets/application.css"
+File.open("app/assets/stylesheets/application.scss", "ab") do |f|
+  f.write <<-EOL
+@import "bootstrap-sprockets";
+@import "bootstrap";
+  EOL
+end
+
+File.open("app/assets/javascripts/application.js", "ab") do |f|
+  f.write <<-EOL
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require_tree .
+  EOL
 end
 
 run "bundle install"
