81558a8ee529c0b501dfa7add561db64fdf18a4c
Asfand Yar Qazi <ayqazi@gmail.com>
2015-12-11 11:26:40 +0000

    Update gem versions: rails, bootstrap-sass

--- template.rb
+++ template.rb
@@ -4,7 +4,7 @@ end
 
 run ">Gemfile"
 add_source "https://rubygems.org"
-gem "rails", "~> 4.2.4"
+gem "rails", "~> 4.2.5"
 gem "pg"
 
 gem "haml-rails"
@@ -12,7 +12,7 @@ gem "haml-rails"
 gem "uglifier", ">= 1.3.0"
 gem "jquery-rails"
 gem "sass-rails", "~> 5.0"
-gem "bootstrap-sass", "~> 3.3.4"
+gem "bootstrap-sass", "~> 3.3.6"
 
 
 gem_group :development, :test do
